d7b181a1b377f9b9e941d9fdb01bf2107f6a5e42
Sunny Kumar <55615100+sunny8080@users.noreply.github.com>
2023-04-21 17:23:31 +0530

    Add seeders for Bootcamp model

---
+++ seeder.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const mongoose = require('mongoose')
+const colors = require('colors');
+const dotenv = require('dotenv')
+
+// load env variables
+dotenv.config({ path: './config/config.env' })
+
+// load models
+const Bootcamp = require('./models/Bootcamp')
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI);
+
+// read json files
+const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
+
+// import to DB
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+    console.log(`Data imported`.green.inverse);
+    process.exit();
+  } catch (err) {
+    console.log(err);
+    process.exit();
+  }
+}
+
+
+
+// delete from db
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    console.log(`Data deleted`.red.inverse);
+    process.exit();
+  } catch (err) {
+    console.log(err);
+    process.exit();
+  }
+}
+
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-d') {
+  deleteData();
+}
