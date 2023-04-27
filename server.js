1bcaeecf84f8cc8f52e101795bd81f8161dd3517
Sunny Kumar <55615100+sunny8080@users.noreply.github.com>
2023-04-28 04:33:50 +0530

    User register, login & bcrypt, JWT

--- server.js
+++ server.js
@@ -27,10 +27,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Route files
 const bootcamps = require("./routes/bootcamps");
 const courses = require("./routes/courses");
+const auth = require("./routes/auth");
 
 // mount bootcamps routers
 app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
+app.use("/api/v1/auth", auth);
 
 
 // error handler
